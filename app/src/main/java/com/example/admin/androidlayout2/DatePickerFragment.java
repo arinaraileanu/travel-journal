1096a3c5c4c5972db9dc949196657304dc00b888
Arina Raileanu <raileanu.arina1@gmail.com>
2018-11-26 01:00:19 +0200

    Button text changes to selected date and added ripple effect to button.

--- app/src/main/java/com/example/admin/androidlayout2/DatePickerFragment.java
+++ app/src/main/java/com/example/admin/androidlayout2/DatePickerFragment.java
@@ -4,12 +4,19 @@ import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.widget.Button;
 import android.widget.DatePicker;
 
 import java.util.Calendar;
 
 public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
-
+    Button btn;
+    public void setButton(Button btn) {
+        this.btn = btn;
+    }
+    public Button getButton(Button btn) {
+        return btn;
+    }
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the current date as the default date in the picker
@@ -24,5 +31,10 @@ public class DatePickerFragment extends DialogFragment implements DatePickerDial
 
     public void onDateSet(DatePicker view, int year, int month, int day) {
         // Do something with the date chosen by the user
+        populateSetDate(btn, year, month, day);
+    }
+    public static void populateSetDate(Button btn, int year, int month, int day) {
+        String text = day+"/"+month+"/"+year;
+        btn.setText(text);
     }
 }
--- app/src/main/java/com/example/admin/androidlayout2/MainActivity.java
+++ app/src/main/java/com/example/admin/androidlayout2/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +14,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void showDatePickerDialog(View v) {
+        Button button = (Button) findViewById(v.getId());
         DialogFragment newFragment = new DatePickerFragment();
+        ((DatePickerFragment) newFragment).setButton(button);
         newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 }
